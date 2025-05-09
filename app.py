ad8a8dbd212a2f5bfcb4ddfdc7d4012529adb8bd
Zafar <z.tahirov@gmail.com>
2025-05-09 01:20:17 -0700

    Make flask static

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template, request, url_for
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.context_processor
 def inject_globals():
     from datetime import datetime
-    return {"request": request, "now": datetime.utcnow(), "url_for": url_for}
+    return {
+        "request": request,
+        "now": datetime.utcnow(),
+        "url_for": url_for,
+        "base_url": "https://maida.ai"
+    }
 
 @app.route("/")
 def home():
